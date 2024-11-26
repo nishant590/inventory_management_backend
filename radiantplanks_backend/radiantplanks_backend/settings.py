b9b27e65a0ded2f1e6ead10174499e538075be01
nishant590 <nishant@pruthatek.com>
2024-11-26 15:30:02 +0530

    static path added

--- radiantplanks_backend/radiantplanks_backend/settings.py
+++ radiantplanks_backend/radiantplanks_backend/settings.py
@@ -164,8 +164,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory for collected static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Directory for project-specific static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
