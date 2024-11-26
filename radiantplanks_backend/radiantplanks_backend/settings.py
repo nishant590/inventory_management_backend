dbef8310c4f6fa1d3a270da018fd7eabde3fd69c
nishant590 <nishant@pruthatek.com>
2024-11-26 17:29:06 +0530

    settings fixed

--- radiantplanks_backend/radiantplanks_backend/settings.py
+++ radiantplanks_backend/radiantplanks_backend/settings.py
@@ -166,9 +166,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory for collected static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Directory for collected static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Directory for project-specific static files
+    os.path.join(BASE_DIR, 'staticfiles'),  # Directory for project-specific static files
 ]
 
 # Default primary key field type
